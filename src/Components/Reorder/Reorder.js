87bc53adba646af784003e9572297d7a61691e9a
Dan Alvarez <danalvarez5280@gmail.com>
2018-01-19 11:23:59 -0700

    add reorder page, it pulls from medications stored in state.

--- src/Components/Reorder/Reorder.js
+++ src/Components/Reorder/Reorder.js
@@ -9,11 +9,27 @@ class Reorder extends Component {
   constructor(props) {
     super();
     this.state = {
-      medications: props.addMed
+      medications: props.medications
     };
   }
 
+  displayMedications() {
+
+    return this.state.medications.map((elem, index) => {
+      return (
+        <div>
+          <h4>{elem.medicationName}</h4>
+          <p>{elem.dose} mg</p>
+          <p>{elem.refills} Refills</p>
+          <p>Next Fill: N/A</p>
+        </div>
+      )
+    })
+  }
+
   render() {
+    console.log('currentMeds', this.props.medications)
+    const currentMeds = this.displayMedications()
     return <div className="med-info">
         <div className="new-order">
           <Link className="nav-confirm" to="/">
@@ -21,8 +37,22 @@ class Reorder extends Component {
           </Link>
           <h3>ReOrder</h3>
         </div>
+        {currentMeds}
       </div>;
   }
 }
 
 export default MainContainer(Reorder);
+
+// undefined;
+// docName: "d";
+// docNumber: "d";
+// dose: "d";
+// medNum: 0;
+// medicationName: "d";
+// pharmacy: undefined;
+// quantity: "d";
+// refills: "d";
+// storedMeds: undefined;
+// supply: "d";
+// usePreviousDocInfo: f;
